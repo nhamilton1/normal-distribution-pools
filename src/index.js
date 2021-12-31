1dfea1d7aee77aab955ccf6649e4125b196f28eb
nhamilton1 <nickh3592@gmail.com>
2021-12-31 16:02:56 -0500

    adds components, helpers, utils, for normal distribution comp

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.less';
 import App from './App';
 import { QueryClientProvider, QueryClient } from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
+// import { ReactQueryDevtools } from 'react-query/devtools'
 
 const queryClient = new QueryClient()
 
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools initialIsOpen={false}/>
+      {/* <ReactQueryDevtools initialIsOpen={false}/> */}
     </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
