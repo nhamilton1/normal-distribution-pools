e629215d3e16557867da81a83d04aa4c31bc7c5a
nhamilton1 <nickh3592@gmail.com>
2021-12-31 21:17:18 -0500

    removes console log

--- src/components/NormalDistribution.js
+++ src/components/NormalDistribution.js
@@ -22,7 +22,7 @@ const NormalDistribution = () => {
   if (isLoading) {
     return <Skeleton paragraph={{ rows: 10 }} />
   }
-  console.log(poolBlockCounterPerDay)
+  
   const dataPoints = Object.values(poolBlockCounterPerDay).sort((a, b) => a - b)
 
   const lowerBound = Math.min(...dataPoints), upperBound = Math.max(...dataPoints);
