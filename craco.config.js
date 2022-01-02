c300d0d73bd483e0934951824aeab596b081f088
nhamilton1 <nickh3592@gmail.com>
2022-01-02 17:34:56 -0500

    going back to npm

--- craco.config.js
+++ craco.config.js
@@ -1,43 +1,21 @@
-const CracoLessPlugin = require('craco-less');
-
-const lessModifyVars = {};
+const CracoLessPlugin = require("craco-less");
 
 module.exports = {
-    plugins: [
-        {
-            plugin: CracoLessPlugin,
-            options: {
-                lessLoaderOptions: {
-                    lessOptions: {
-                        modifyVars: lessModifyVars,
-                        javascriptEnabled: true,
-                    },
-                },
-                modifyLessRule: function (lessRule, _context) {
-                    lessRule.test = /\.less$/;
-                    lessRule.exclude = /\.module\.less$/;
-                    return lessRule;
-                },
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: {
+              "@primary-color": "#1DA57A",
+              "@link-color": "#1DA57A",
+              "@border-radius-base": "2px",
             },
+            javascriptEnabled: true,
+          },
         },
-        {
-            plugin: CracoLessPlugin,
-            options: {
-                lessLoaderOptions: {
-                    lessOptions: {
-                        modifyVars: lessModifyVars,
-                        javascriptEnabled: true,
-                    },
-                },
-                modifyLessRule: function (lessRule, _context) {
-                    lessRule.test = /\.module\.less$/;
-                    lessRule.exclude = undefined;
-                    return lessRule;
-                },
-                cssLoaderOptions: {
-                    modules: { localIdentName: '[local]_[hash:base64:5]' },
-                },
-            },
-        },
-    ],
+      },
+    },
+  ],
 };
