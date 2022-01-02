32f1d19500903fa80dea31df35dc2c43ae6f1c2c
nhamilton1 <nickh3592@gmail.com>
2022-01-01 23:00:46 -0500

    heroku test

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
+const path = require("path");
+const app = express();
 
 const poolRouter = require('./poolRouter')
 
@@ -11,6 +13,7 @@ server.use(cors())
 
 server.use('/', poolRouter)
 
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
